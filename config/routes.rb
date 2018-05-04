36ff24c2218c0818130993759904320b747ee742
François MATHIEU <maps_red@yahoo.fr>
2018-05-04 11:27:30 +0200

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  # resources :pokemon, only: %i[index show]
+
+  # resources :pokemon do
+  #   member do
+  #     get :index
+  #   end
+  # end
+
+end
