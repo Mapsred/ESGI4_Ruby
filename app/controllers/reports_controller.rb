adf5596cb5ddedc0dc2c7b194134bd5e68ce1dfe
Jérémy Haudry <jeremy-haudry@hotmail.fr>
2018-06-12 20:20:19 +0200

    #7 Optimisations reports_controller, boucles inutiles

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -18,43 +18,38 @@ class ReportsController < ApplicationController
   def create
     @report = Report.new(report_params)
     report_params.inspect
-    respond_to do |format|
-      if @report.save
-        flash[:success] = 'Rapport "' + report_params[:name] + '" ajouté avec succès !'
-        format.html { redirect_to reports_url }
-      else
-        format.html { render :new }
-      end
+    if @report.save
+      flash[:success] = 'Rapport "' + report_params[:name] + '" ajouté avec succès !'
+      redirect_to reports_url
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        flash[:success] = 'Rapport "' + report_params[:name] + '" mis à jour avec succès !'
-        format.html { redirect_to @report}
-      else
-        format.html { render :edit }
-      end
+    if @report.update(report_params)
+      flash[:success] = 'Rapport "' + report_params[:name] + '" mis à jour avec succès !'
+      redirect_to reports_url
+    else
+      render :edit
     end
   end
 
   def destroy
     @report.destroy
-    respond_to do |format|
-      # flash[:success] = 'Rapport "' + report_params[:name] + '" effacé'
-      format.html { redirect_to reports_url }
-    end
+      flash[:success] = 'Rapport effacé avec succès !'
+      redirect_to reports_url
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_report
-      @report = Report.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def report_params
-      params.require(:report).permit(:name, :description, :astronaut_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def report_params
+    params.require(:report).permit(:name, :description, :astronaut_id)
+  end
 end
