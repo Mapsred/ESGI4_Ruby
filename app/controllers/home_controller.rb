3b138e1f89b6095955f7b23df9543c167fb5811b
AxelE <axel.evrard@outlook.fr>
2018-07-08 18:34:03 +0200

    Clean icon, create footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,13 @@ class HomeController < ApplicationController
   def index
     @expedition = Expedition.last_opened
   end
+  def credit
+    render :'home/credit'
+  end
+  def condition
+    render :'home/condition'
+  end
+  def partner
+    render :'home/partner'
+  end
 end
